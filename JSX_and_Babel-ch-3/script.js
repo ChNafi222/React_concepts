fb1ffa4cebd0e47d622016efc889fb2e5734806e
ChNafi222 <chnafi379@gmail.com>
2024-08-08 23:20:44 +0500

    commit

--- JSX_and_Babel-ch-3/script.js
+++ JSX_and_Babel-ch-3/script.js
@@ -4,7 +4,7 @@
 //--- conversion--Babel can convert JSX (JavaScript XML) into regular JavaScript. JSX is a syntax extension commonly used with React that allows you to write HTML-like code within JavaScript files. Babel handles the transformation of JSX into the appropriate React.createElement calls, which are necessary for rendering React components.
 
 
-// babbel converts the jsx into javascript
+// babbel is a compiler that converts next the next generation Javascript and jsx into normal javascript
 
 const user = 'munaf'
 const h2 = <h2> Hello jsx <b>munaf</b> </h2>
