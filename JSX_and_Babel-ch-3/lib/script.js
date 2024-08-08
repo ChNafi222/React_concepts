0abbbda34340e5d47d17063eca365aa8a447e2fc
ChNafi222 <chnafi379@gmail.com>
2024-08-08 23:17:53 +0500

    commit

---
+++ JSX_and_Babel-ch-3/lib/script.js
@@ -0,0 +1,6 @@
+// babbel converts the jsx into javascript
+
+const user = 'munaf';
+const h2 = /*#__PURE__*/React.createElement("h2", null, " Hello jsx ", /*#__PURE__*/React.createElement("b", null, "munaf"), " ");
+const root = ReactDOM.createRoot(document.querySelector('#root'));
+root.render(h2);
--- JSX_and_Babel-ch-3/script.js
+++ JSX_and_Babel-ch-3/script.js
@@ -1,8 +1,14 @@
+//-- definition-- Babel is an essential tool for modern JavaScript development that enables developers to write code using the latest features while ensuring compatibility with older environments. It helps improve code quality, maintainability, and future-proofing, making it a valuable asset in any JavaScript project.
+
+
+//--- conversion--Babel can convert JSX (JavaScript XML) into regular JavaScript. JSX is a syntax extension commonly used with React that allows you to write HTML-like code within JavaScript files. Babel handles the transformation of JSX into the appropriate React.createElement calls, which are necessary for rendering React components.
+
+
 // babbel converts the jsx into javascript
 
 const user = 'munaf'
 const h2 = <h2> Hello jsx <b>munaf</b> </h2>
 
 const root = ReactDOM.createRoot(document.querySelector('#root'))
-
+     
 root.render(h2)
