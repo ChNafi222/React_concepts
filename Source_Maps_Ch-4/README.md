38df3ac2d57411b3076546197b4d8f2108d1b445
ChNafi222 <chnafi379@gmail.com>
2024-08-11 11:58:14 +0500

    commit

---
+++ Source_Maps_Ch-4/README.md
@@ -0,0 +1,7 @@
+Source Maps :- Source maps
+
+- help in debugging
+- when lengthy and complex code used to locate the elements easily
+
+to create Source maps - "babel ./script.js -d lib --source-maps" ; and then run command npm run build
+this will create a script.js.maps in the lib folder
---
+++ Source_Maps_Ch-4/script.js
@@ -0,0 +1,65 @@
+// const h2  = React.createElement('h2', {className : 'subheading'},'Hello React')
+
+
+// creating element
+// const h2  = {
+//     "$$typeof": Symbol.for("react.element"), //$$typeof for security, can't be transferred through json
+// props:  { 
+//     className: "subheading", 
+//     id : 'subtitle',
+//     children: "Hello React" },
+// ref: null,
+// type: "h2",
+
+// }
+
+
+// another example 
+const container = React.createElement(
+    'div',{
+        className : 'container' , id : 'container'
+    },
+    [
+    React.createElement('section', {
+            key:1
+        },
+        'The library for web and native user interfaces'
+    ),
+    React.createElement(
+        'img',{
+            key: 2, src:'./react_logo.png'
+        },
+    ),
+
+    // second section
+    React.createElement('section',{
+        key:3
+    },
+    [
+        React.createElement('form',{
+            key:1
+        },[
+            React.createElement('div',{className : 'input_group' },
+            [
+                React.createElement('label',{key:1, htmlFor: 'username'}, 'Username'),
+                React.createElement('input',{key:2, id:'username'}),
+            ]
+            ),
+    
+        ]
+    ),
+    ]
+    )
+    
+    ]
+
+)
+
+
+// Wait until the DOM is fully loaded
+document.addEventListener('DOMContentLoaded', () => {
+    const root = ReactDOM.createRoot(document.querySelector('#root'));
+    // Render the h2 element
+    root.render(container);
+})
+// reactDOM renders the on webpage
